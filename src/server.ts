321b6f0b9da8c7eb05c0404155282b1d61d015a3
ShvetsovYura <shvetsov.yura@hotmail.com>
2021-12-08 16:20:14 +0300

    add compression

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import { resolve } from "path";
 import express, { Express } from "express";
 import { ssrHtmlRenderMiddleware } from "ssrHtmlRenderMiddleware";
+import compression from "compression";
 
 const app: Express = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(compression());
+}
+
 app
   .use(express.static(resolve(__dirname, "../dist")))
   .use(express.static(resolve(__dirname, "../static")));
